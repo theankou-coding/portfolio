7e17fc5831861ced1280d9f937d4166681868452
theankou-coding <loem.theankou.2822@rupp.edu.kh>
2025-07-07 20:57:05 +0700

    add my project

--- src/components/sub/hero-content.tsx
+++ src/components/sub/hero-content.tsx
@@ -49,12 +49,12 @@ export const HeroContent = () => {
           Mobile, and Software development. Check out my projects and skills.
         </motion.p>
 
-        <motion.a
+        {/* <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
           Learn more
-        </motion.a>
+        </motion.a> */}
       </div>
 
       <motion.div
